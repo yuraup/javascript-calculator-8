6aae93a155a873a420a0c7c348aaf1a0792fae7f
yuraup <o721@naver.com>
2025-10-17 13:41:10 +0900

    feat: 입출력 형식 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    let input = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.");
+
+    const result = input;
+    Console.print(`결과 : ${result}`);
+  }
 }
 
 export default App;
